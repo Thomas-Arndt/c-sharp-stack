d7f4f7648b1f96e46a48e9bcd132badb1e9001df
Thomas Arndt <arndtt42@gmail.com>
2022-03-16 15:51:35 -0700

    Assignment to priactice boxing and unboxing variables into a list with type casting

---
+++ language-fundamentals/BoxingUnboxing/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BoxingUnboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<object> objectList = new List<object>();
+            objectList.Add(7);
+            objectList.Add(28);
+            objectList.Add(-1);
+            objectList.Add(true);
+            objectList.Add("chair");
+            int sum = 0;
+            foreach(object obj in objectList) {
+                Console.WriteLine(obj);
+                if(obj is int) {
+                    sum += (int)obj;
+                }
+            }
+            Console.WriteLine($"The sum is {sum}");
+        }
+    }
+}
