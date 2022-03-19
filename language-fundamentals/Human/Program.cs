0a300e08d258ed151012b0ecc601e3ca30919738
Thomas Arndt <arndtt42@gmail.com>
2022-03-19 16:33:13 -0700

    Assignment to work with oop in c#

---
+++ language-fundamentals/Human/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Human
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Human h1 = new Human("Bob");
+            Human h2 = new Human("Bill", 10, 9, 12, 250);
+            Console.WriteLine(h1.Attack(h2));
+        }
+
+    }
+
+    class Human {
+        public string Name;
+        public int Strength;
+        public int Intelligence;
+        public int Dexterity;
+        private int health { get;set; }
+
+        public Human(string Name) {
+            this.Name = Name;
+            this.Strength = 3;
+            this.Intelligence = 3;
+            this.Dexterity = 3;
+            this.health = 100;
+        }
+
+        public Human(string Name, int Strength, int Intelligence, int Dexterity, int health) {
+            this.Name = Name;
+            this.Strength = Strength;
+            this.Intelligence = Intelligence;
+            this.Dexterity = Dexterity;
+            this.health = health;
+        }
+
+        public int Attack(Human target) {
+            target.health = target.health - (this.Strength*5);
+            return target.health;
+        }
+    }
+}
