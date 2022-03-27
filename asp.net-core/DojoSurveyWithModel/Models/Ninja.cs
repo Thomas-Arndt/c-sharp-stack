95aa864950be445f8122104d6e597e269e449d48
Thomas Arndt <arndtt42@gmail.com>
2022-03-26 17:39:19 -0700

    Assignment to practice binding form data to a model then displaying model data using View Model

---
+++ asp.net-core/DojoSurveyWithModel/Models/Ninja.cs
@@ -0,0 +1,7 @@
+public class Ninja
+{
+    public string name { get;set; }
+    public string location { get;set; }
+    public string language { get;set; }
+    public string comment { get;set; }
+}
